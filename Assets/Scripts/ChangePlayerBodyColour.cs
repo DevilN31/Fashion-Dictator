35a15bf1df15d509a9a28e3a13058c7f57f6a8e6
Nati <33567785+DevilN31@users.noreply.github.com>
2018-03-28 13:18:49 +0300

    The trunk
    IT'S ALIVE!!!

---
+++ Assets/Scripts/ChangePlayerBodyColour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangePlayerBodyColour : MonoBehaviour {
+
+    public Material playerBodyPart;
+    public Text changeText;
+
+	public void ChangeBodyMaterial()
+    {
+        playerBodyPart.color = Color.blue;
+        changeText.text = "Right";
+    }
+}
---
+++ Assets/Scripts/ChangePlayerBodyToWrong.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangePlayerBodyToWrong : MonoBehaviour {
+
+    public Material playerBodyPart;
+    public Text changeText;
+
+    public void ChangeBodyMaterialToWrong()
+    {
+        playerBodyPart.color = Color.white;
+        changeText.text = "Wrong";
+    }
+}
