81b8dc45283d6d62f60496d6becb05364285de87
MrTrainee <33583323+MrTrainee@users.noreply.github.com>
2018-07-01 22:12:37 +0300

    quality of like
    better names and better studdseses

--- Assets/Scripts/SeasonLogic.cs
+++ Assets/Scripts/SeasonLogic.cs
@@ -7,7 +7,7 @@ public enum Seasons { Spring, Summer, Fall, Winter }
 
 public class SeasonLogic : MonoBehaviour
 {
-    public Seasons currentSeason;
+    private Seasons currentSeason;
 
     public bool DetermineCorrectLength(List<Clothes> clothes)
     {
@@ -26,36 +26,60 @@ public class SeasonLogic : MonoBehaviour
         {
             case Seasons.Spring:
                 if (longClothes == 1 && shortClothes == 3)
+                {
+                    Debug.Log("Returned true for spring 1 long 3 short");
                     return true;
+                }
                 else
+                {
+                    Debug.Log("Returned false for spring");
                     return false;
+                }
             case Seasons.Summer:
                 if (longClothes == 0 && shortClothes == 4)
+                {
+                    Debug.Log("Returned true for summer 0 long 4 short");
                     return true;
+                }
                 else
+                {
+                    Debug.Log("Returned false for summer");
                     return false;
+                }
             case Seasons.Fall:
                 if (longClothes == 3 && shortClothes == 1)
+                {
+                    Debug.Log("Returned true for fall 3 long 1 short");
                     return true;
+                }
                 else
+                {
+                    Debug.Log("Returned false for fall");
                     return false;
+                }
             case Seasons.Winter:
                 if (longClothes == 4 && shortClothes == 0)
+                {
+                    Debug.Log("Returned true for winter 4 long 0 short");
                     return true;
+                }
                 else
+                {
+                    Debug.Log("Returned false for winter");
                     return false;
+                }
             default:
-                Debug.Log("Somehow default activated");
+                Debug.Log("Somehow default activated when the current season was checked in the switch");
                 return false;
         }
     }
 
-    public void SetCurrentSeasonEnum(Seasons newSeason)
+    public void SetCurrentSeasonWithEnum(Seasons newSeason)
     {
         currentSeason = newSeason;
     }
 
-    public void SetCurrentSeasonString(string newSeason)
+    public void SetCurrentSeasonWithString(string newSeason)
     {
         switch (newSeason)
         {
@@ -72,8 +96,13 @@ public class SeasonLogic : MonoBehaviour
                 currentSeason = Seasons.Fall;
                 break;
             default:
-                Debug.Log("Somehow default activated, did you forget the capital letter?");
+                Debug.Log("Somehow default activated when setting season, did you forget the capital letter?");
                 break;
         }
+    }
+
+    public Seasons GetCurrentSeason()
+    {
+        return currentSeason;
     }
 }
