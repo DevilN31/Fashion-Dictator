4908afb0d6db9e37ea73cc9104142327e616f8fa
Nati <33567785+DevilN31@users.noreply.github.com>
2018-07-07 22:03:33 +0300

    UI has beun!

---
+++ Assets/BeSync/Tool/BeSyncSetMaterial.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class BeSyncSetMaterial : MonoBehaviour {
+
+
+	public GameObject target;
+
+
+
+	public Material material;
+
+    /*
+
+	void OnEnable(){
+		BeSyncEvent.OnPlayModeAgainEvent += OnImportComplete;
+	}
+	void OnDisable(){
+		BeSyncEvent.OnPlayModeAgainEvent -= OnImportComplete;
+	}
+
+    */
+
+	public void OnImportComplete(string str){
+
+		if(target != null){
+
+			Renderer[] ren = target.GetComponentsInChildren<Renderer>();
+			Debug.Log(ren.Length);
+			foreach(Renderer r in ren){
+				r.material = material;
+			}
+		}
+	}
+
+}
---
+++ Assets/Scripts/UI/Clock.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour {
+
+    public int ratio;
+    public float newRatio;
+    int timePassed;
+    Vector3 targetAngles;
+
+    // Use this for initialization
+    void Start ()
+    {
+        timePassed = (int)Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       timePassed = (int)Time.time;
+        ratio = 60 - ((int)timePassed/60);
+
+        if(ratio >= 0)
+        Debug.Log(timePassed);
+
+        if (ratio > newRatio)
+        {
+            newRatio = ratio;
+           // Rotate(newRatio);
+        }
+
+        
+    }
+
+    void Rotate(float ratio)
+    {
+
+        targetAngles = transform.eulerAngles + 60f * Vector3.forward;
+        print(targetAngles);
+
+    }
+}
