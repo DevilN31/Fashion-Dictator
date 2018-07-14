1b7bf344f53adeaea155d61d01ac2a80d44da518
MrTrainee <33583323+MrTrainee@users.noreply.github.com>
2018-07-15 01:39:27 +0300

    made some stuff for the player and game manager scripts
    codes to kill random players and then main player after 2 tries

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,17 +4,65 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public Player myplayer;
+    public Player myPlayer;
+    public Player[] playerArray;
+
+    public int tryNumber = 0;
+    
 
 	// Use this for initialization
 	void Start ()
     {
+        myPlayer = GetComponentInChildren<Player>();
+        myPlayer.isActivePlayer = true;
 
-	}
+        myPlayer.GetComponent<SeasonLogic>().SetCurrentSeasonWithString("Summer");
+
+        FindPlayers();
+
+        foreach (Player player in playerArray)
+        {
+            if (player != null)
+            Debug.Log(player);
+        }
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+        if (myPlayer.allowKillCheck == true)
+        {
+            if (tryNumber < 2)
+                KillRandomPlayer();
+            else
+                AttemptKillCurrentPlayer();
+        }
+        myPlayer.allowKillCheck = false;
+    }
+    // create an array of all the characters that are not the current playable player
+    public void FindPlayers()
+    {
+        playerArray = FindObjectsOfType<Player>();
+    }
+    // disables players that are wearing wrong clothes, skips dead players
+    public void KillRandomPlayer()
+    {
+        foreach (Player player in playerArray)
+        {
+            if (player.didPlayerWin == false && player.isActivePlayer == false && player.gameObject.activeSelf)
+            {
+                player.gameObject.SetActive(false);
+                Debug.Log("killed: " + player);
+                tryNumber++;
+                break;
+            }
+        }
+    }
+    // kills player
+    public void AttemptKillCurrentPlayer()
+    {
+        if (myPlayer.didPlayerWin == false)
+            myPlayer.gameObject.SetActive(false);
+        Debug.Log("killed: " + myPlayer);
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,23 +4,70 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    List<Clothes> clothes = new List<Clothes>();
+    private Clothes[] clothes;
 
-	// Use this for initialization
-	void Start ()
+    private SeasonLogic playerSeasonLogic;
+
+    public bool doesHaveCorrectSeasonals;
+    public bool doesHaveCorrectColors;
+    public bool didPlayerWin;
+    public bool isActivePlayer;
+    public bool allowKillCheck;
+
+    // Use this for initialization
+    void Start ()
     {
         WornClothes();
-        Debug.Log(clothes);
+
+        foreach (Clothes article in clothes)
+        {
+            Debug.Log(article);
+        }
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-
-	}
+        if(Input.GetKeyUp(KeyCode.C))
+        {
+            getSeasonValidation();
+            Debug.Log("For Player: " + this);
+            getColorValidation();
+            Debug.Log("For Player: " + this);
+            SetVictoryCondition();
+            allowKillCheck = true;
+        }
+    }
 
     public void WornClothes()
     {
-        clothes.AddRange(GetComponentsInChildren<Clothes>());
+        clothes = GetComponentsInChildren<Clothes>();
+    }
+
+    public void getSeasonValidation()
+    {
+        doesHaveCorrectSeasonals = GetComponent<SeasonLogic>().DetermineCorrectLength(clothes);
+
+    }
+
+    public void getColorValidation()
+    {
+        doesHaveCorrectColors = GetComponent<ColorCheck>().CheckColors(clothes);
+
+    }
+
+    public void SetVictoryCondition()
+    {
+        if (doesHaveCorrectSeasonals && doesHaveCorrectColors)
+        {
+            didPlayerWin = true;
+            Debug.Log(this + " Cannot Die!");
+        }
+
+        else
+        {
+            didPlayerWin = false;
+            Debug.Log(this + " May Die!");
+        }
     }
 }
